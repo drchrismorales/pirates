0934b2be9492f5ce54ab0f5bb6ac1de3d45dd059
chris <chris@none.non>
2022-11-17 18:43:39 -0500

    Drowned pirate changes:
    2-5 pirates appear, or a pirate captain plus 1-4 others
    
    "You" are attacked, not "the ship" since the event can occur while the pirates are exploring islands.

--- game/events/drowned_pirates.py
+++ game/events/drowned_pirates.py
@@ -13,16 +13,20 @@ class DrownedPirates (event.Event):
         result = {}
         result["message"] = "the drowned pirates are defeated!"
         monsters = []
+        min = 2
+        uplim = 6
         if random.randrange(2) == 0:
+            min = 1
+            uplim = 5
             monsters.append(Drowned("Pirate captain"))
             monsters[0].speed = 1.2*monsters[0].speed
             monsters[0].health = 2*monsters[0].health
-        n_appearing = random.randrange(1,5)
+        n_appearing = random.randrange(min, uplim)
         n = 1
         while n <= n_appearing:
             monsters.append(Drowned("Drowned pirate "+str(n)))
             n += 1
-        announce ("The ship is attacked by a crew of drowned pirates!")
+        announce ("You are attacked by a crew of drowned pirates!")
         Combat(monsters).combat()
         result["newevents"] = [ self ]
         return result
