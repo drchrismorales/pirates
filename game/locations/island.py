757701f4ea4a2629ac51560904d2937bb0cb1389
chris <chris@none.non>
2022-11-18 15:15:02 -0500

    Put two items in the demo island forest, to demo taking items while exploring.
    Also foreshadows the man-eating macaque encounter, which is currently the hardest in the game.

--- game/locations/island.py
+++ game/locations/island.py
@@ -3,6 +3,8 @@ from game import location
 from game import config
 from game.display import announce
 from game.events import *
+from game.items import Cutlass
+from game.items import Flintlock
 
 class Island (location.Location):
 
@@ -58,15 +60,55 @@ class Trees (location.SubLocation):
         self.verbs['south'] = self
         self.verbs['east'] = self
         self.verbs['west'] = self
+
+        # Include a couple of items and the ability to pick them up, for demo purposes
+        self.verbs['take'] = self
+        self.item_in_tree = Cutlass()
+        self.item_in_clothes = Flintlock()
+
         self.event_chance = 50
         self.events.append(man_eating_monkeys.ManEatingMonkeys())
         self.events.append(drowned_pirates.DrownedPirates())
 
     def enter (self):
-        announce ("You walk into the small forest on the island. Nothing around here looks very edible.")
+        edibles = False
+        for e in self.events:
+            if isinstance(e, man_eating_monkeys.ManEatingMonkeys):
+                edibles = True
+        #The description has a base description, followed by variable components.
+        description = "You walk into the small forest on the island."
+        if edibles == False:
+             description = description + " Nothing around here looks very edible."
+        
+        #Add a couple items as a demo. This is kinda awkward but students might want to complicated things.
+        if self.item_in_tree != None:
+            description = description + " You see a " + self.item_in_tree.name + " stuck in a tree."
+        if self.item_in_clothes != None:
+            description = description + " You see a " + self.item_in_clothes.name + " in a pile of shredded clothes on the forest floor."
+        announce (description)
     
     def process_verb (self, verb, cmd_list, nouns):
         if (verb == "south" or verb == "north" or verb == "east" or verb == "west"):
             config.the_player.next_loc = self.main_location.locations["beach"]
-
-
+        #Handle taking items. Demo both "take cutlass" and "take all"
+        if verb == "take":
+            if self.item_in_tree == None and self.item_in_clothes == None:
+                announce ("You don't see anything to take.")
+            elif len(cmd_list) > 1:
+                at_least_one = False #Track if you pick up an item, print message if not.
+                item = self.item_in_tree
+                if item != None and (cmd_list[1] == item.name or cmd_list[1] == "all"):
+                    announce ("You take the "+item.name+" from the tree.")
+                    config.the_player.add_to_inventory([item])
+                    self.item_in_tree = None
+                    config.the_player.go = True
+                    at_least_one = True
+                item = self.item_in_clothes
+                if item != None and (cmd_list[1] == item.name or cmd_list[1] == "all"):
+                    announce ("You pick up the "+item.name+" out of the pile of clothes. ...It looks like someone was eaten here.")
+                    config.the_player.add_to_inventory([item])
+                    self.item_in_clothes = None
+                    config.the_player.go = True
+                    at_least_one = True
+                if at_least_one == False:
+                    announce ("You don't see one of those around.")
