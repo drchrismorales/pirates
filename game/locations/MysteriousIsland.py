f89fe8e730b9a74d11d1c3528c692d3c8d521408
jpsalis <jpsalis@outlook.com>
2023-12-02 16:35:28 -0500

    Work on updating Mysterious Island.

--- game/locations/MysteriousIsland.py
+++ game/locations/MysteriousIsland.py
@@ -41,84 +41,92 @@ class Beach (location.SubLocation):
     def __init__ (self, m):
         super().__init__(m)
         self.name = "beach"
-        self.verbs['go'] = self
-        self.nouns['north'] = self
-        self.nouns['west'] = self
-        self.nouns['ship'] = self
-        self.verbs['exit'] = self
-        self.event_chance = 50
-        self.events.append (seagull.Seagull())
+        self.verbs['go'] = self # Override player go method to add more directions
+        #self.event_chance = 50
+        #self.events.append (seagull.Seagull())
 
     def enter (self):
-        announce ("You are on the beach. Your ship is anchored to the dock.")
+        description = "You are on the island's beach. Your ship is anchored to the dock."
+        description += "\nTo your north is a hill. To your west is a forest."
+        announce (description)
 
     def process_verb (self, verb, cmd_list, nouns):
-        if (verb == "go"):
-            print(nouns)
-            if 'ship' in nouns or 'exit' in nouns:
-                config.the_player.next_loc = config.the_player.ship
-                config.the_player.visiting = False
-            elif 'west' in nouns:
-                config.the_player.next_loc = self.main_location.locations["ruins"]
-            elif 'cliff' in nouns: 
-                config.the_player.next_loc = self.main_location.locations["cliff"]
-            else:
-                print(nouns)
-
-        else:
-            announce("Command not recognized.")
-
-
+        if verb == 'go':
+            config.the_player.go = True
+            if len(cmd_list) > 1:
+                if cmd_list[1] == "north":
+                    config.the_player.next_loc = self.main_location.locations["cliff"]
+                if cmd_list[1] == "west":
+                    config.the_player.next_loc = self.main_location.locations["ruins"]
+                elif cmd_list[1] == 'south' or cmd_list[1] == 'east':
+                    announce("Nothing but open ocean.")
+                elif cmd_list[1] == 'ship' or cmd_list[1] == 'exit' or cmd_list[1] == 'dock':
+                    announce ("You return to your ship.")
+                    config.the_player.next_loc = config.the_player.ship
+                    config.the_player.visiting = False
+                
+            
 class Cliff (location.SubLocation):
     def __init__ (self, m):
         super().__init__(m)
         self.name = "cliff"
-        self.verbs['go'] = self
         self.verbs['take'] = self
-        self.nouns['south'] = self
-        self.nouns['ladder'] = self
 
 
     def enter (self):
-        description = "You ascend the cliff to the north."
+        description = "You ascend the hill to the plateau."
+        #description = "Climbing the ladder, "
+        description = "."
+        description = "Many buildings seem worse for wear, many if not all abandoned."
         announce (description)
 
     def process_verb (self, verb, cmd_list, nouns):
-        print(nouns)
-        print(cmd_list)
-        if verb == "south":
-            config.the_player.next_loc = self.main_location.locations["beach"]
-        if verb == 'ladder':
-            announce('You don\'t see a ladder.')
-
+        if verb == 'go':
+            if len(cmd_list) > 1:
+                if cmd_list[1] == "south":
+                    config.the_player.next_loc = self.main_location.locations["beach"]
+                    config.the_player.go = True
+                elif cmd_list[1] == "ladder":
+                    config.the_player.next_loc = self.main_location.locations["sanctuary"]
+                    config.the_player.go = True
+                elif cmd_list[1] == 'west' or cmd_list[1] == 'east' or cmd_list[1] == 'north':
+                    announce("Nothing but open ocean.")
+                """
+                elif cmd_list[1] == 'ship' or cmd_list[1] == 'exit' or cmd_list[1] == 'dock':
+                    announce ("You return to your ship.")
+                    config.the_player.next_loc = config.the_player.ship
+                    config.the_player.visiting = False
+                """
+                
 
 class Ruins (location.SubLocation):
     def __init__ (self, m):
         super().__init__(m)
         self.name = "ruins"
         self.verbs['go'] = self
-        self.nouns['ruins'] = self
-        self.nouns['east'] = self
-
-        # Include a couple of items and the ability to pick them up, for demo purposes
+        #self.verbs['take'] = self
 
     def enter (self):
-        edibles = False
-        for e in self.events:
-            if isinstance(e, man_eating_monkeys.ManEatingMonkeys):
-                edibles = True
         #The description has a base description, followed by variable components.
-        description = "An abandoned town presides here."
+        description = "You and your remaining team make their way through the dense foliage. The sunlight beams brightly through a clearing ahead."
+        description += "\n"
+
+        #TODO: Add makeshift weapons
 
         #TODO: add check for user coming from the dungeon for the first time.
 
-        #Add a couple items as a demo. This is kinda awkward but students might want to complicated things.
         announce (description)
 
     def process_verb (self, verb, cmd_list, nouns):
         print(nouns)
-        if verb == "south":
-            config.the_player.next_loc = self.main_location.locations["beach"]
+        if verb == 'go':
+            if cmd_list[1] == "east":
+                config.the_player.next_loc = self.main_location.locations["beach"]
+            elif cmd_list[1] == "temple" or cmd_list[1] == 'ruins':
+                # TODO: Add condition, ladder not visible if sanctuary not visited
+                config.the_player.next_loc = self.main_location.locations["sanctuary"]
+        
+
         #Handle taking items. Demo both "take cutlass" and "take all"
         if verb == "take":
             if self.item_in_tree == None and self.item_in_clothes == None:
